39d6530892c94c2b2ac73a310b8af93c5c7aad3e
plexion <46572320+plexiondev@users.noreply.github.com>
2023-01-21 17:51:08 +0000

    export components

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/* components */
+
+export { default as Avatar } from './components/Avatar.svelte';
+export { default as Badge } from './components/Badge.svelte';
+export { default as Button } from './components/Button.svelte';
+export { default as File } from './components/File.svelte';
