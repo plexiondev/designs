be8d3ae636feedcd3a0b53df35783eff407acfe8
plexion <46572320+plexiondev@users.noreply.github.com>
2023-01-21 15:57:48 +0000

    BADGES

---
+++ src/components/Badge.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let label = '';
+    export let colour = 'default';
+</script>
+
+<div class="badge {colour}">
+    {label}
+</div>
+
+<style>
+    .badge {
+        display: inline-flex;
+        align-items: center;
+        font-weight: 500;
+    }
+    .badge:before {
+        content: '';
+        display: inline-flex;
+        background-color: currentColor;
+        width: 7px;
+        height: 7px;
+        border-radius: 50px;
+        margin-right: 5px;
+    }
+
+    .badge.green { color: var(--green); }
+    .badge.red   { color: var(--red);   }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Button from '../components/Button.svelte';
     import Avatar from '../components/Avatar.svelte';
+	import Badge from '../components/Badge.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -8,46 +9,59 @@
 
 <div class="big-grid">
     <span>
-        <h3>Buttons</h3>
+        <span>
+            <h3>Buttons</h3>
 
-        <h4>Default</h4>
-        <Button as="a" href="https://plexion.dev" size="small">Button</Button>
-        <Button as="a" size="medium">Button</Button>
-        <Button as="a" size="large">Button</Button>
-        <Button as="a" size="hero">Button</Button>
+            <h4>Default</h4>
+            <Button as="a" href="https://plexion.dev" size="small">Button</Button>
+            <Button as="a" size="medium">Button</Button>
+            <Button as="a" size="large">Button</Button>
+            <Button as="a" size="hero">Button</Button>
 
-        <h4>Primary</h4>
-        <Button as="a" size="small" colour="primary">Button</Button>
-        <Button as="a" size="medium" colour="primary">Button</Button>
-        <Button as="a" size="large" colour="primary">Button</Button>
-        <Button as="a" size="hero" colour="primary">Button</Button>
+            <h4>Primary</h4>
+            <Button as="a" size="small" colour="primary">Button</Button>
+            <Button as="a" size="medium" colour="primary">Button</Button>
+            <Button as="a" size="large" colour="primary">Button</Button>
+            <Button as="a" size="hero" colour="primary">Button</Button>
 
-        <h4>Danger</h4>
-        <Button as="a" size="small" colour="danger">Button</Button>
-        <Button as="a" size="medium" colour="danger">Button</Button>
-        <Button as="a" size="large" colour="danger">Button</Button>
-        <Button as="a" size="hero" colour="danger">Button</Button>
+            <h4>Danger</h4>
+            <Button as="a" size="small" colour="danger">Button</Button>
+            <Button as="a" size="medium" colour="danger">Button</Button>
+            <Button as="a" size="large" colour="danger">Button</Button>
+            <Button as="a" size="hero" colour="danger">Button</Button>
 
-        <h4>Outline</h4>
-        <Button as="a" size="small" colour="outline">Button</Button>
-        <Button as="a" size="medium" colour="outline">Button</Button>
-        <Button as="a" size="large" colour="outline">Button</Button>
-        <Button as="a" size="hero" colour="outline">Button</Button>
+            <h4>Outline</h4>
+            <Button as="a" size="small" colour="outline">Button</Button>
+            <Button as="a" size="medium" colour="outline">Button</Button>
+            <Button as="a" size="large" colour="outline">Button</Button>
+            <Button as="a" size="hero" colour="outline">Button</Button>
+        </span>
     </span>
     <span>
-        <h3>Avatar</h3>
+        <span>
+            <h3>Avatar</h3>
 
-        <h4>Default</h4>
-        <Avatar size="large" src="https://avatars.githubusercontent.com/u/46572320?v=4" />
-        <Avatar size="medium" src="https://avatars.githubusercontent.com/u/46572320?v=4" />
-        <Avatar size="small" src="https://avatars.githubusercontent.com/u/46572320?v=4" />
-        <Avatar size="extra-small" src="https://avatars.githubusercontent.com/u/46572320?v=4" />
+            <h4>Default</h4>
+            <Avatar size="large" src="https://avatars.githubusercontent.com/u/46572320?v=4" />
+            <Avatar size="medium" src="https://avatars.githubusercontent.com/u/46572320?v=4" />
+            <Avatar size="small" src="https://avatars.githubusercontent.com/u/46572320?v=4" />
+            <Avatar size="extra-small" src="https://avatars.githubusercontent.com/u/46572320?v=4" />
 
-        <h4>Circular</h4>
-        <Avatar size="large" circle src="https://avatars.githubusercontent.com/u/46572320?v=4" />
-        <Avatar size="medium" circle src="https://avatars.githubusercontent.com/u/46572320?v=4" />
-        <Avatar size="small" circle src="https://avatars.githubusercontent.com/u/46572320?v=4" />
-        <Avatar size="extra-small" circle src="https://avatars.githubusercontent.com/u/46572320?v=4" />
+            <h4>Circular</h4>
+            <Avatar size="large" circle src="https://avatars.githubusercontent.com/u/46572320?v=4" />
+            <Avatar size="medium" circle src="https://avatars.githubusercontent.com/u/46572320?v=4" />
+            <Avatar size="small" circle src="https://avatars.githubusercontent.com/u/46572320?v=4" />
+            <Avatar size="extra-small" circle src="https://avatars.githubusercontent.com/u/46572320?v=4" />
+        </span>
+    </span>
+    <span>
+        <span>
+            <h3>Badge</h3>
+
+            <Badge label="Badge"></Badge>
+            <Badge label="Badge" colour="green"></Badge>
+            <Badge label="Badge" colour="red"></Badge>
+        </span>
     </span>
 </div>
 
@@ -56,6 +70,11 @@
         display: grid;
         grid-template-columns: repeat(auto-fill, minmax(380px, 1fr));
         padding: 0 20px;
+        gap: 20px;
+    }
+    .big-grid > span {
+        display: flex;
+        flex-direction: column;
         gap: 20px;
     }
 </style>
