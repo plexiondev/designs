24fb69736303ea8b56c1b28e8513c2596b94e4b1
plexion <46572320+plexiondev@users.noreply.github.com>
2023-01-25 19:29:09 +0000

    display value of `ProgressBar` on hover parity with plexion.dev

--- src/components/ProgressBar.svelte
+++ src/components/ProgressBar.svelte
@@ -2,13 +2,18 @@
     export let maximum = 100;
     export let current = 75;
 
+    export let title = 'Value'; // tooltip title
+
+    import { tippy } from 'svelte-tippy';
+    import 'tippy.js/dist/tippy.css';
+
     export let style: string | 'default' | 'rating1' | 'rating2' = 'default';
 
     let progress = (current / maximum) * 100;
 </script>
 
 <div class="progress-bar {style}">
-    <span class="progress" style="width: {progress}%" />
+    <span class="progress" style="width: {progress}%" use:tippy={{content: `<strong>${title}</strong> ${current}`, arrow: false, allowHTML: true}} />
 </div>
 
 <style>
@@ -21,10 +26,15 @@
         border-radius: 10px;
     }
     .progress-bar .progress {
-        background-color: var(--l2);
+        --background: var(--l2);
+        background-color: var(--background);
         display: block;
         height: var(--height);
         border-radius: 10px;
+        transition: box-shadow 0.15s;
+    }
+    .progress-bar .progress:hover {
+        box-shadow: 0px 0px 6px 0px var(--background);
     }
 
     /* rating1 */
@@ -32,7 +42,7 @@
         background-color: var(--red);
     }
     .progress-bar:is(.rating1, .rating2) .progress {
-        background-color: var(--green);
+        --background: var(--green);
     }
 
     /* rating2 */
