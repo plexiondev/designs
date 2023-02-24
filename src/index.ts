30d3205400057e171e3612ac457c896009525c40
plexion <46572320+plexiondev@users.noreply.github.com>
2023-02-24 16:37:15 +0000

    trying things!

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 /* components */
 
-export { default as Avatar } from './components/Avatar.svelte';
-export { default as AvatarCard } from './components/AvatarCard.svelte';
-export { default as Badge } from './components/Badge.svelte';
-export { default as Button } from './components/Button.svelte';
-export { default as File } from './components/File.svelte';
-export { default as FileList } from './components/FileList.svelte';
-export { default as InfoColumn } from './components/InfoColumn.svelte';
-export { default as InfoRow } from './components/InfoRow.svelte';
-export { default as Panel } from './components/Panel.svelte';
-export { default as PanelContainer } from './components/PanelContainer.svelte';
-export { default as ProgressBar } from './components/ProgressBar.svelte';
-export { default as ProjectCard } from './components/ProjectCard.svelte';
-export { default as ProjectCardList } from './components/ProjectCardList.svelte';
-export { default as RatingSpread } from './components/RatingSpread.svelte';
-export { default as RoleList } from './components/RoleList.svelte';
-export { default as StatisticItem } from './components/StatisticItem.svelte';
-export { default as StatusDot } from './components/StatusDot.svelte';
+export { default as Avatar } from './lib/components/Avatar.svelte';
+export { default as AvatarCard } from './lib/components/AvatarCard.svelte';
+export { default as Badge } from './lib/components/Badge.svelte';
+export { default as Button } from './lib/components/Button.svelte';
+export { default as File } from './lib/components/File.svelte';
+export { default as FileList } from './lib/components/FileList.svelte';
+export { default as InfoColumn } from './lib/components/InfoColumn.svelte';
+export { default as InfoRow } from './lib/components/InfoRow.svelte';
+export { default as Panel } from './lib/components/Panel.svelte';
+export { default as PanelContainer } from './lib/components/PanelContainer.svelte';
+export { default as ProgressBar } from './lib/components/ProgressBar.svelte';
+export { default as ProjectCard } from './lib/components/ProjectCard.svelte';
+export { default as ProjectCardList } from './lib/components/ProjectCardList.svelte';
+export { default as RatingSpread } from './lib/components/RatingSpread.svelte';
+export { default as RoleList } from './lib/components/RoleList.svelte';
+export { default as StatisticItem } from './lib/components/StatisticItem.svelte';
+export { default as StatusDot } from './lib/components/StatusDot.svelte';
